a0061ca37acdd1c0541a43a962c7ee6ecaab0d62
KBohyeon <145645063+KBohyeon@users.noreply.github.com>
2024-02-05 20:11:41 +0900

    first commit

---
+++ web.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, url_for, request
+ 
+app = Flask(__name__)
+    
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    print("one")
+    if request.method == 'POST':
+        print("aa")
+        return redirect(url_for('test'))
+    return render_template('main.html')
+
+@app.route('/test')
+def test():
+    return "test page"
+ 
+if __name__ == '__main__':
+    app.run(host="192.168.0.82", port=80, debug=True)
