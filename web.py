0666a2b1598a641b029599869422e18ebc5e0ea4
KBohyeon <145645063+KBohyeon@users.noreply.github.com>
2024-02-21 15:45:28 +0900

    p

--- web.py
+++ web.py
@@ -2,21 +2,23 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-#test 1
-#testestdddasdasdds
-#test3
-    
 @app.route('/', methods=['GET', 'POST'])
-def main():
-    print("one")
-    if request.method == 'POST':
-        print("aa")
-        return redirect(url_for('test'))
+def main_page():
     return render_template('main.html')
 
-@app.route('/test')
-def test():
-    return "test page"
+@app.route('/test1', methods=['GET', 'POST'])
+def test1():
+    if request.method == 'POST':
+        return "test page 1"
+    else:
+        return redirect(url_for('main'))
+
+@app.route('/test2', methods=['GET', 'POST'])
+def test2():
+    if request.method == 'POST':
+        return "test page 2"
+    else:
+        return redirect(url_for('main'))
 
 if __name__ == '__main__':
     app.run(host="192.168.0.82", port=80, debug=True)
