b8e6fb0de1f2f09740fcb5f867cfed9f9b7b71f4
KBohyeon <145645063+KBohyeon@users.noreply.github.com>
2024-03-11 16:49:47 +0900

    1212

--- web.py
+++ web.py
@@ -4,21 +4,26 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
-    return render_template('main.html')
+    return render_template('start.html')
+
+@app.route('/start', methods=['GET', 'POST'])
+def start():
+    if request.method == 'POST':
+        app.start = True
+        return render_template('main.html')
+    return redirect(url_for('main_page'))
 
 @app.route('/test1', methods=['GET', 'POST'])
 def test1():
     if request.method == 'POST':
-        return "test page 1"
-    else:
-        return redirect(url_for('main'))
-
+        app.test2 = 1  
+    pass
+    
 @app.route('/test2', methods=['GET', 'POST'])
 def test2():
     if request.method == 'POST':
-        return "test page 2"
-    else:
-        return redirect(url_for('main'))
+        app.test2 = 2
+    pass
 
 if __name__ == '__main__':
     app.run(host="192.168.0.82", port=80, debug=True)
